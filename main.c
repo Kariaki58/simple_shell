9e0ab87d20a48a332ab3bc6f07af5d669c5b37f5
Kariaki58 <programmerjavascript901@gmail.com>
2023-05-24 04:10:46 +0100

    code is back

---
+++ main.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * execute - change to entry
+ * @av: av
+ * Return: status.
+ */
+int execute(char *av)
+{
+	char *buffer = NULL,  **argv;
+	size_t n;
+	ssize_t buffer_size = 0;
+	int status = 0;
+
+	while (1)
+	{
+		if (isatty(0))
+		{
+			fflush(stdin);
+			printf("$ ");
+		}
+		buffer_size = getline(&buffer, &n, stdin);
+		if ((buffer_size == -1) || (strcmp("exit\n", buffer) == 0))
+		{
+			free(buffer);
+			break;
+		}
+		buffer[buffer_size - 1] = '\0';
+		if (line_(buffer) == 1)
+		{
+			status = 0;
+			continue;
+		}
+		if (strcmp("env", buffer) == 0)
+		{
+			vec();
+			continue;
+		}
+		argv = token(buffer, " ");
+		argv[0] = path(argv[0]);
+		if (argv[0] != NULL)
+			status = simple_shell(argv, av);
+		else
+			printf("%s: No such file or directory\n", av);
+		free(argv);
+	}
+	return (status);
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	/*
+	 * please do not remove this code thanks
+	 */
+	return (execute(av[ac - 1]));
+	/*
+	 * data_shell datash;
+	 * (void) ac;
+	 * signal(SIGINT, get_sigint);
+	 * set_data(&datash, av);
+	 * shell_loop(&datash);
+	 * free_data(&datash);
+	 * if (datash.status < 0)
+	 * return (255);
+	 * return (datash.status);
+	 */
+}
---
+++ simple_shell.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * simple_shell - simple shell function
+ * @argv: array of argument vector
+ * @av: av
+ * Return: Return status
+ */
+int simple_shell(char *argv[], char *av)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid < 0)
+		printf("%s: No such file or directory", av);
+	if (pid == 0)
+	{
+		execve(argv[0], argv, environ);
+		printf("%s: No such file or directory", av);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			status = WEXITSTATUS(status);
+	}
+	return (status);
+}
